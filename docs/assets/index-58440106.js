6fea5a22284826edecbf3c0b2fc21f8706cfb28a
docentedev <docente.dev@gmail.com>
2024-02-15 15:23:25 -0300

    Deployed at jueves, 15 de febrero de 2024, 15:23:25 -03

---
+++ docs/assets/index-58440106.js
@@ -0,0 +1,26 @@
+import{a as t,j as r,F as p}from"./jsx-runtime-fd534075.js";import{M as a,b as m,C as s}from"./index-5c3150a5.js";import{C as c,D as l}from"./index.stories-d74d7eac.js";import{a as d}from"./index-5c67f7a8.js";import{u as n}from"./index-09eae0a8.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-bea88fb1.js";import"../sb-preview/runtime.js";import"./index-af5faa0b.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./index-5bc492e7.js";import"./index-d49d0512.js";import"./index-ac710f84.js";/* empty css              */import"./index-964f190d.js";import"./index-528324a4.js";import"./index-e60269c1.js";import"./index-089ca2ca.js";import"./index-0f348ff1.js";import"./index-1335fada.js";import"./index-287b9535.js";import"./index-07b437fe.js";import"./index-89aa9bbc.js";import"./index-65ca6685.js";import"./index-4cd4725f.js";import"./index-d2c7445a.js";import"./index-3b419322.js";import"./index-36b10e2f.js";import"./index-c29d1954.js";import"./index-354a9bc7.js";import"./index-dc21327b.js";import"./index-3f0062ba.js";import"./index-d70482d4.js";import"./index-02210d9f.js";import"./index-6bf6aae2.js";import"./index-7ea187ef.js";import"./index-886936c1.js";import"./index-c29c2bb6.js";import"./index-63de68db.js";function i(e){const o=Object.assign({h2:"h2",em:"em",strong:"strong",p:"p",code:"code"},n(),e.components);return r(p,{children:[t(a,{of:c}),`
+`,r(o.h2,{id:"paginationes-deprecado",children:["PaginationEs ",t(o.em,{children:t(o.strong,{children:"Deprecado"})})]}),`
+`,t(d,{variant:"warning",children:"Deprecado"}),`
+`,r(o.p,{children:["Este componente sera eliminado el ",t(o.em,{children:t(o.strong,{children:"01-01-2024"})})," ya que el componente ",t(o.code,{children:"<Pagination>"})," puede hacer lo mismo y es mas extensible."]}),`
+`,r(o.p,{children:["Adaptar props que recibe ",t(o.code,{children:"<PaginationEs>"})," para utilizar ",t(o.code,{children:"<Pagination>"})]}),`
+`,t(m,{code:`
+const page = Math.floor(props.from / props.size + 1);
+const totalPages = Math.ceil((props.total || 0) / props.size);
+return (
+    <Pagination
+        onPage={props.onPage}
+        onNext={props.onNext}
+        onBack={props.onBack}
+        onPageSize={props.onPageSize}
+        isFirst={page === 1}
+        numberOfElements={props.currentRow}
+        page={page}
+        isLast={page === totalPages}
+        pageSize={props.size}
+        totalElements={props.total}
+        totalPages={totalPages}
+        variant="alternative"
+    />
+);
+`}),`
+`,t(s,{of:l})]})}function to(e={}){const{wrapper:o}=Object.assign({},n(),e.components);return o?t(o,Object.assign({},e,{children:t(i,e)})):i(e)}export{to as default};
