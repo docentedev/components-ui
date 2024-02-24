cb042873210b347a2628180a0f5be8bdde8b5bf7
docentedev <docente.dev@gmail.com>
2024-02-24 05:26:43 -0300

    Deployed at sábado, 24 de febrero de 2024, 05:26:43 -03

---
+++ docs/assets/index-183f4504.js
@@ -0,0 +1,16 @@
+import{a as t,j as m,F as n}from"./jsx-runtime-fd534075.js";import{M as s,C as p,d as a,b as c}from"./index-849cd17e.js";import{C as d,D as u}from"./index.stories-705f957d.js";import"./index-6d2f8f90.js";import{u as e}from"./index-09eae0a8.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-3c175a14.js";import"../sb-preview/runtime.js";import"./index-af5faa0b.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";/* empty css              */import"./index-e9e4b522.js";import"./index-150f24d0.js";import"./index-03eabb6a.js";import"./index-e60269c1.js";import"./index-94b86141.js";import"./index-6574e2dc.js";import"./index-65ca6685.js";import"./index-4cd4725f.js";import"./index-162d25a7.js";import"./index-3b419322.js";import"./index-36b10e2f.js";import"./index-b6ef4225.js";import"./index-9e68d7ed.js";import"./index-d70482d4.js";import"./index-a1e9148f.js";import"./index-649ebd0f.js";import"./index-02210d9f.js";import"./index-6bf6aae2.js";import"./index-7ea187ef.js";import"./index-4c811791.js";import"./index-ca884482.js";import"./index-63de68db.js";function r(o){const i=Object.assign({h2:"h2"},e(),o.components);return m(n,{children:[t(s,{of:d}),`
+`,t(i.h2,{id:"radiobutton",children:"RadioButton"}),`
+`,t(p,{of:u}),`
+`,t(a,{}),`
+`,t(c,{code:`
+const [checked, setChecked] = useState(false);
+const [disabled, setDisabled] = useState(false);
+return (
+    <RadioButton
+        disabled={disabled}
+        checked={checked}
+        onClick={setChecked}
+        label="Radio Button"
+    />
+);
+`})]})}function V(o={}){const{wrapper:i}=Object.assign({},e(),o.components);return i?t(i,Object.assign({},o,{children:t(r,o)})):r(o)}export{V as default};
---
+++ docs/assets/index.stories-cf2684c0.js
@@ -0,0 +1,26 @@
+import{j as t,a as e}from"./jsx-runtime-fd534075.js";import{A as c}from"./index-e9e4b522.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";import"./index-150f24d0.js";import"./index-03eabb6a.js";import"./index-e60269c1.js";const m={title:"Accordion",component:c,tags:["autodocs"],parameters:{layout:"centered"}},a={args:{items:[{label:"¿Cómo puedo crear una cuenta?",content:'Para crear una cuenta, haz clic en el botón "Registrarse" en la esquina superior derecha de la página.'},{label:"¿Cómo puedo crear una cuenta?",content:'Para crear una cuenta, haz clic en el botón "Registrarse" en la esquina superior derecha de la página.'},{label:"¿Cómo puedo crear una cuenta?",content:t("div",{children:[e("p",{children:"algo"}),e("p",{children:"algo"}),e("p",{children:"algo"}),e("p",{children:"algo"}),e("p",{children:"algo"})]})}],active:-1,allowedAllClosed:!0,size:"xs",gap:.125,iconSize:12,iconPosition:"right"}};var n,r,o;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      label: '¿Cómo puedo crear una cuenta?',
+      content: 'Para crear una cuenta, haz clic en el botón "Registrarse" en la esquina superior derecha de la página.'
+    }, {
+      label: '¿Cómo puedo crear una cuenta?',
+      content: 'Para crear una cuenta, haz clic en el botón "Registrarse" en la esquina superior derecha de la página.'
+    }, {
+      label: '¿Cómo puedo crear una cuenta?',
+      content: <div>
+                        <p>algo</p>
+                        <p>algo</p>
+                        <p>algo</p>
+                        <p>algo</p>
+                        <p>algo</p>
+                    </div>
+    }],
+    active: -1,
+    allowedAllClosed: true,
+    size: 'xs',
+    gap: 0.125,
+    iconSize: 12,
+    iconPosition: "right"
+  }
+}`,...(o=(r=a.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const h=["Default"];export{a as Default,h as __namedExportsOrder,m as default};
