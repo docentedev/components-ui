6255a28a5f5bc67b433af974cbcd2ca5c28a139f
docentedev <docente.dev@gmail.com>
2024-03-08 04:15:51 -0300

    Deployed at viernes,  8 de marzo de 2024, 04:15:51 -03

---
+++ docs/welcome/index.stories.d.ts
@@ -0,0 +1,4 @@
+import type { Meta, StoryObj } from '@storybook/react';
+export declare const WelcomeStory: StoryObj;
+declare const ExportMeta: Meta<typeof WelcomeStory>;
+export default ExportMeta;
