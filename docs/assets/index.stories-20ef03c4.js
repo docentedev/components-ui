e208bbc86313d86e2f5383d510a70b01c0dc055f
docentedev <docente.dev@gmail.com>
2024-03-08 04:18:39 -0300

    Deployed at viernes,  8 de marzo de 2024, 04:18:39 -03

---
+++ docs/assets/index.stories-20ef03c4.js
@@ -0,0 +1,5 @@
+import{B as t,b as a}from"./index-94b86141.js";import"./jsx-runtime-fd534075.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";const i={title:"Brand",component:t,tags:["autodocs"],argTypes:{size:{control:"number",description:"Tamaño de componente"},name:{options:Object.keys(a),control:{type:"select"},value:"Cencosud",description:"Icono"}}},e={args:{size:100}};var o,r,s;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: 100
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,i as default};
