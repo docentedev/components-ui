0297df52206982d39c9e2cf15a15e3e6ef589322
docentedev <docente.dev@gmail.com>
2024-03-08 04:20:43 -0300

    Deployed at viernes,  8 de marzo de 2024, 04:20:43 -03

---
+++ docs/assets/index.stories-99e25656.js
@@ -0,0 +1,24 @@
+import{a as e}from"./jsx-runtime-fd534075.js";import{s as d}from"./index-f27f94eb.js";import{I as t}from"./index-12e24dad.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";/* empty css              */import"./index-edbe8c20.js";import"./index-a2221cae.js";import"./index-675c71d2.js";import"./index-9e68d7ed.js";import"./index-02210d9f.js";import"./index-b5044b3e.js";import"./index-95d22c3b.js";import"./index-94b86141.js";import"./index-af5faa0b.js";import"./index-95353159.js";import"./index-65ca6685.js";import"./index-4cd4725f.js";import"./index-541d37a9.js";import"./index-3b419322.js";import"./index-36b10e2f.js";import"./index-b6ef4225.js";import"./index-d70482d4.js";import"./index-d88dedce.js";import"./index-0051ad11.js";import"./index-6bf6aae2.js";import"./index-7ea187ef.js";import"./index-63de68db.js";const G={title:"Tabs",component:d,tags:["autodocs"],argTypes:{variant:{control:"select",options:["primary","secondary"],description:"Variante de la tab por defecto primary"},index:{control:"number",description:"Indice de la tab por defecto 0, si se utiliza este prop se debe utilizar onChange para actualizar el estado del componente con una propiedad de estado en el componente padre"}},render:o=>e(p,{...o})},p=o=>{const l=o.variant==="secondary"?{backgroundColor:"#ECF6FF",padding:"10px"}:{};return e("div",{style:l,children:e(d,{...o})})},n={args:{tabs:[{label:"Tab 1",content:e("div",{children:"Contenido del Tab 1"}),icon:e(t,{name:"Box"}),badge:"100",disabled:!1},{label:"Tab 2",content:e("div",{children:"Contenido del Tab 2"}),icon:e(t,{name:"Box"}),badge:void 0,disabled:!0},{label:"Tab 3",content:e("div",{children:"Contenido del Tab 3"})},{label:"Tab 4",icon:e(t,{name:"Box"}),content:e("div",{children:"Contenido del Tab 4"})}]}};var a,i,r;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    tabs: [{
+      label: 'Tab 1',
+      content: <div>Contenido del Tab 1</div>,
+      icon: <Icon name="Box" />,
+      badge: '100',
+      disabled: false
+    }, {
+      label: 'Tab 2',
+      content: <div>Contenido del Tab 2</div>,
+      icon: <Icon name="Box" />,
+      badge: undefined,
+      disabled: true
+    }, {
+      label: 'Tab 3',
+      content: <div>Contenido del Tab 3</div>
+    }, {
+      label: 'Tab 4',
+      icon: <Icon name="Box" />,
+      content: <div>Contenido del Tab 4</div>
+    }]
+  }
+}`,...(r=(i=n.parameters)==null?void 0:i.docs)==null?void 0:r.source}}};const H=["Default"];export{n as Default,H as __namedExportsOrder,G as default};
