a8dfc63a1c278a4f8d3df835fcf7570e77df6734
docentedev <docente.dev@gmail.com>
2024-03-12 13:04:20 -0300

    Deployed at martes, 12 de marzo de 2024, 13:04:20 -03

--- docs/assets/index.stories-3a829c72.js
+++
@@ -1,8 +0,0 @@
-import{a as e}from"./index-2681c571.js";import"./jsx-runtime-fd534075.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";import"./index.utils-04f34053.js";import"./index-ef379c3a.js";import"./index-a2847f9e.js";import"./index-12e24dad.js";import"./index-ca44b1cf.js";import"./index-9e68d7ed.js";import"./index-889d04a6.js";import"./index-d0b2ccd2.js";import"./index-de8952c4.js";import"./index-94b86141.js";import"./index-af5faa0b.js";import"./index-95353159.js";import"./index-65ca6685.js";import"./index-4cd4725f.js";import"./index-541d37a9.js";import"./index-3b419322.js";import"./index-36b10e2f.js";import"./index-b6ef4225.js";import"./index-d70482d4.js";import"./index-d88dedce.js";import"./index-0051ad11.js";import"./index-6bf6aae2.js";import"./index-181649f6.js";import"./index-63de68db.js";const B={title:"Alert",component:e,tags:["autodocs"],args:{variant:"info",title:"This is the title",icon:!0,children:"This is the content of the alert."},argTypes:{variant:{options:["info","success","error","warning"],control:{type:"select"}},theme:{options:["default","light"],control:{type:"select"}}}},t={args:{variant:"info",title:"This is the title",children:"This is the content of the alert.",icon:!0}};var r,i,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    variant: 'info',
-    title: 'This is the title',
-    children: 'This is the content of the alert.',
-    icon: true
-  }
-}`,...(o=(i=t.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const C=["Default"];export{t as Default,C as __namedExportsOrder,B as default};
