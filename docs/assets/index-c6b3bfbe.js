ae6e3d6e1d7cc3050c8fb23e435348b396c424e8
docentedev <docente.dev@gmail.com>
2024-03-12 12:59:29 -0300

    Deployed at martes, 12 de marzo de 2024, 12:59:29 -03

---
+++ docs/assets/index-c6b3bfbe.js
@@ -0,0 +1,16 @@
+import{a as t,j as m,F as n}from"./jsx-runtime-fd534075.js";import{M as s,C as p,d as a,b as c}from"./index-ff0773a2.js";import{C as d,D as u}from"./index.stories-4c328da5.js";import"./index-fcce77eb.js";import{u as e}from"./index-09eae0a8.js";import"./index-0cbcd92a.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-9cc51587.js";import"../sb-preview/runtime.js";import"./index-af5faa0b.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./index-889d04a6.js";import"./index.utils-04f34053.js";import"./index-31e51faa.js";import"./index-a2221cae.js";import"./index-03eabb6a.js";import"./index-675c71d2.js";import"./index-9e68d7ed.js";import"./index-bcd14bec.js";import"./index-95017c5b.js";import"./index-94b86141.js";import"./index-598450d7.js";import"./index-65ca6685.js";import"./index-4cd4725f.js";import"./index-162d25a7.js";import"./index-3b419322.js";import"./index-36b10e2f.js";import"./index-b6ef4225.js";import"./index-d70482d4.js";import"./index-ddbb0a27.js";import"./index-959cc4af.js";import"./index-6bf6aae2.js";import"./index-181649f6.js";import"./index-63de68db.js";function r(o){const i=Object.assign({h2:"h2"},e(),o.components);return m(n,{children:[t(s,{of:d}),`
+`,t(i.h2,{id:"radiobutton",children:"RadioButton"}),`
+`,t(p,{of:u}),`
+`,t(a,{}),`
+`,t(c,{code:`
+const [checked, setChecked] = useState(false);
+const [disabled, setDisabled] = useState(false);
+return (
+    <RadioButton
+        disabled={disabled}
+        checked={checked}
+        onClick={setChecked}
+        label="Radio Button"
+    />
+);
+`})]})}function V(o={}){const{wrapper:i}=Object.assign({},e(),o.components);return i?t(i,Object.assign({},o,{children:t(r,o)})):r(o)}export{V as default};
