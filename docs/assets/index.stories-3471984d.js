22ad2204b988219efb01b4b0923cd434d49d66ba
docentedev <docente.dev@gmail.com>
2024-07-24 16:59:03 -0400

    Deployed at miércoles, 24 de julio de 2024, 16:59:03 -04

---
+++ docs/assets/index.stories-3471984d.js
@@ -0,0 +1,12 @@
+import{a as o,j as i,F as x}from"./jsx-runtime-fd534075.js";import{r as z}from"./index-0cbcd92a.js";import{I as a,a as y,s as I}from"./index-130c2559.js";import{I as S,T as j}from"./index-9a715a1f.js";import{P as C}from"./index-02210d9f.js";import{S as c}from"./index-79378b34.js";import"./_commonjsHelpers-de833af9.js";import"./index.utils-3332eaf1.js";import"./index-177d311a.js";import"./Easy-dac9347f.js";import"./index-abb8f9ba.js";import"./index-ae9d3e9c.js";import"./index-af5faa0b.js";import"./index-339673fb.js";import"./index-65ca6685.js";import"./index-be8450de.js";import"./index-7b389540.js";import"./index-36b10e2f.js";import"./index-5eea7179.js";import"./index-d70482d4.js";import"./index-86e4212d.js";import"./index-f05aaf72.js";import"./index-c2412459.js";import"./index-7ea187ef.js";const W={title:"Components/Icon",component:a,tags:["autodocs"],argTypes:{size:{control:{type:"range",min:16,max:256,step:16}},name:{options:Object.keys(y),control:{type:"select"}},color:{control:{type:"color"}},variant:{options:["","rounded-edge-box","tag"],control:{type:"select"}},animation:{options:["","rotate","asc","desc"],control:{type:"select"}}},render:s=>o(v,{...s})},v=s=>{const[n,f]=z.useState(""),h=Object.keys(y).filter(r=>r.toLowerCase().includes(n.toLowerCase()));return i(x,{children:[o(C,{justifyContent:"center",children:o(a,{...s})}),o(c,{}),o(S,{type:"text",placeholder:"Buscar icono",value:n,onChange:r=>f(r.target.value),size:"s",label:"Buscar icono"}),o(c,{}),o("div",{className:I.storiesDisplayIcons,children:h.map(r=>i("div",{children:[o(a,{name:r,size:20,color:"--components-ui-color-dark"}),o(j,{size:12,children:r})]},r))})]})},e={args:{size:48}},t={args:{name:"Spinner",size:48,color:"--components-ui-color-primary",animation:"rotate"}};var p,m,l;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: 48
+  }
+}`,...(l=(m=e.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var d,u,g;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    name: 'Spinner',
+    size: 48,
+    color: '--components-ui-color-primary',
+    animation: 'rotate'
+  }
+}`,...(g=(u=t.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};const X=["Default","Loader"];export{e as Default,t as Loader,X as __namedExportsOrder,W as default};
---
+++ docs/assets/texts.stories-26612d64.js
@@ -0,0 +1,24 @@
+import{j as r,a as t}from"./jsx-runtime-fd534075.js";import{t as b,a as C}from"./index.utils-5ee1497d.js";import{e as h,I as y}from"./index-9a715a1f.js";import{P as x}from"./index-02210d9f.js";import{r as f}from"./index-0cbcd92a.js";import{R as m}from"./index-c2412459.js";import{C as s}from"./index-65ca6685.js";import"./index.utils-3332eaf1.js";import"./index-79378b34.js";import"./index-130c2559.js";import"./index-177d311a.js";import"./Easy-dac9347f.js";import"./index-abb8f9ba.js";import"./index-ae9d3e9c.js";import"./index-af5faa0b.js";import"./_commonjsHelpers-de833af9.js";import"./index-339673fb.js";import"./index-be8450de.js";import"./index-7b389540.js";import"./index-36b10e2f.js";import"./index-5eea7179.js";import"./index-d70482d4.js";import"./index-86e4212d.js";import"./index-f05aaf72.js";import"./index-7ea187ef.js";const w=e=>e.charAt(0).toUpperCase()+e.slice(1),T=e=>{const a=/(lineHeight|size|weight)(\w+)$/,o=e.match(a),n=o?o[1]:"";return n==="lineHeight"?"lineHeight":`font${w(n)}`},_=[{label:"Nombre",key:"title"},{label:"CSS",renderRow:e=>t("pre",{children:e.cssVar})},{label:"Código",renderRow:e=>t(x,{direction:"row",alignItems:"center",gap:.25,children:t("div",{style:{[T(e.tag)]:e.value},children:e.value})})}],v=({title:e,data:a})=>r("div",{children:[t("h4",{children:e}),t(h,{variant:"narrow",data:a,columns:_})]});try{texttable.displayName="texttable",texttable.__docgenInfo={description:"",displayName:"texttable",props:{title:{defaultValue:null,description:"",name:"title",required:!0,type:{name:"string"}},data:{defaultValue:null,description:"",name:"data",required:!0,type:{name:"TokenType[]"}}}}}catch{}const R=e=>{const a=/\.components-ui-text[^{]*/g,o=e.match(a);return o?o.map(n=>{const i=n.replace(".","").trim();return{title:`.${i}`,tag:"Class",cssVar:"--",value:i}}):[]},k=[{label:"Nombre",key:"title"},{label:"Código",renderRow:e=>t(x,{direction:"row",alignItems:"center",gap:.25,children:t("div",{className:e.value,children:"abcABC123"})})}],N=({title:e,data:a})=>{const[o,n]=f.useState(""),g=R(a).filter(c=>c.value.toLowerCase().includes(o.toLowerCase()));return r("div",{children:[e&&t("h4",{children:e}),t(y,{type:"text",placeholder:"Buscar Clase",value:o,onChange:c=>n(c.target.value),size:"s",label:"Buscar Clase"}),t(h,{variant:"narrow",data:g,columns:k,maxHeight:"300px"})]})};try{classtable.displayName="classtable",classtable.__docgenInfo={description:"",displayName:"classtable",props:{title:{defaultValue:null,description:"",name:"title",required:!1,type:{name:"string"}},data:{defaultValue:null,description:"",name:"data",required:!0,type:{name:"string"}}}}}catch{}const l=()=>t("div",{children:r(m,{children:[t(s,{col:12,children:t("h2",{children:"Tokens"})}),r(s,{col:12,children:[t(m,{children:r(s,{xl:12,xxl:4,children:[t("h2",{children:"Clases de textos"}),r("p",{children:["La clases que estilizan los textos se generan automaticamente bajo el siguiente formato: ",t("strong",{children:".components-ui-text-<lineHeight>-<size>-<fontWeight>"}),", con todas las posibles combinaciones como se muestra a continuación."]}),t(N,{data:b})]})}),r(m,{children:[t(s,{col:12,children:t("h3",{children:"Textos"})}),C.map((e,a)=>t(s,{xl:6,xxl:4,children:t(v,{title:e.title,data:e.data})},a))]})]})]})});l.storyName="Tokens";const S=()=>t(l,{}),ee={title:"Docs/Tokens/Texts",component:S,argTypes:{backgroundColor:{control:{disable:!0}},onClick:{control:{disable:!0}}}};var d,p,u;l.parameters={...l.parameters,docs:{...(d=l.parameters)==null?void 0:d.docs,source:{originalSource:`() => <div>
+        <Row>
+            <Col col={12}>
+                <h2>Tokens</h2>
+            </Col>
+            <Col col={12}>
+                <Row>
+                    <Col xl={12} xxl={4}>
+                        <h2>Clases de textos</h2>
+                        <p>La clases que estilizan los textos se generan automaticamente bajo el siguiente formato: <strong>{\`.components-ui-text-<lineHeight>-<size>-<fontWeight>\`}</strong>, con todas las posibles combinaciones como se muestra a continuación.</p>
+                        <ClassTable data={textsClasses} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col col={12}>
+                        <h3>Textos</h3>
+                    </Col>
+                    {texts.map((item, index) => <Col xl={6} xxl={4} key={index}>
+                            <TextsTable title={item.title} data={item.data} />
+                        </Col>)}
+                </Row>
+            </Col>
+        </Row>
+    </div>`,...(u=(p=l.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const te=["WelcomeStory"];export{l as WelcomeStory,te as __namedExportsOrder,ee as default};
